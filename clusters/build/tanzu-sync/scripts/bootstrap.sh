8ab715e09418e3db7b11077de5cbeabbba6f584b
x95castle1 <x95castle1@yahoo.com>
2023-04-13 09:54:37 -0500

    Upgrade Build Cluster to TAP 1.5.0

--- clusters/build/tanzu-sync/scripts/bootstrap.sh
+++ clusters/build/tanzu-sync/scripts/bootstrap.sh
@@ -8,28 +8,18 @@ function usage() {
   echo "- INSTALL_REGISTRY_HOSTNAME -- hostname of OCI Registry containing TAP packages"
   echo "- INSTALL_REGISTRY_USERNAME -- username of account to OCI Registry containing TAP packages"
   echo "- INSTALL_REGISTRY_PASSWORD -- password of account to OCI Registry containing TAP packages"
-  echo "- KAPP_KUBECONFIG_CONTEXT -- kubectl context name for the target cluster"
   echo ""
 }
 
-for envvar in INSTALL_REGISTRY_USERNAME INSTALL_REGISTRY_PASSWORD INSTALL_REGISTRY_HOSTNAME KAPP_KUBECONFIG_CONTEXT ; do
-  if [[ -z ${envvar} ]]; then
-    usage
-    echo "Expected env var ${envvar} to be set, but was not."
-    exit 1
-  fi
-done
+error_msg="Expected env var to be set, but was not."
+: "${INSTALL_REGISTRY_USERNAME?$error_msg}"
+: "${INSTALL_REGISTRY_PASSWORD?$error_msg}"
+: "${INSTALL_REGISTRY_HOSTNAME?$error_msg}"
 
-kubectl --context="${KAPP_KUBECONFIG_CONTEXT}"  \
-  apply \
+kubectl apply \
   -f <(ytt -f tanzu-sync/bootstrap/ \
            -v registry.hostname="${INSTALL_REGISTRY_HOSTNAME}" \
            -v registry.username="${INSTALL_REGISTRY_USERNAME}" \
            -v registry.password="${INSTALL_REGISTRY_PASSWORD}" \
       ) $@
-           
-echo ""
-echo "Next steps:"
-echo ""
-echo "$  ./tanzu-sync/scripts/configure.sh"
-echo ""
+
